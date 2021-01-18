6283e2f0aff17fb6eed9dcf4e3855bd907b06b9a
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2021-01-18 10:00:04 +0800

    New encoding method

--- index.js
+++ index.js
@@ -26,36 +26,50 @@ function Start() {
 	}
 }
 
-//Encodes the SVG, I am planning on re-making this to save file size.
 function EncodeSVG(InputFilePath) {
 	try {
 		var InputData = fs.readFileSync(InputFilePath);
-	} catch {
+	} catch(err) {
 		console.error("Failed to read file!");
+		console.error(err);
 		process.exit(1);
 	}
+	//Convert the file to a hex string and create the start of the SVG.
 	var HexString = InputData.toString('hex');
-	var HexArray = new Array(Math.ceil(HexString.length / 8));
-	for (var i = 0, charsLength = HexString.length; i < charsLength; i += 8) {
-		HexArray[i/8] = HexString.substring(i, i + 8);
-	}
 	var OutSVG = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-	<svg height="258" width="` + (HexArray.length) + `" viewBox="258, ` + (HexArray.length) + `" xmlns="http://www.w3.org/2000/svg">
-		<line x1="0" y1="257" x2="` + (HexArray.length) + `" y2="257" style="stroke:rgb(0,128,255); stroke-width:1">
-		</line>`;
-	HexArray.forEach(function(CurrentHexBlock, HexIndex) {
+	<svg height="2560" width="2560" viewBox="2560, 2560" xmlns="http://www.w3.org/2000/svg" style="background:{BGColour}">
+	<polyline points="`;
+	var BGColour = "#" + Math.floor(Math.random() * 16777215).toString(16).substr(4).repeat(4);
+	var SizeCounter = 0;
+	var CurrentHexBlock = ""
+	var HexIndex = 0
+	//Go through every 4 characters, convert them into a decimal number and add the points to a polyline.
+	for (var i = 0, charsLength = HexString.length; i < charsLength; i += 4) {
+		//I earlier split the string into a massive array and went through that.
+		//This method saves a ton of memory.
+		CurrentHexBlock = HexString.substring(i, i + 4);
+		HexIndex = i / 4
+		SizeCounter++;
+		//Split the polyline every 1000 characters to reduce lag.
+		if (SizeCounter % 250 == 0) {
+			OutSVG += `" style="stroke:#` + Number(HexIndex * 16777215).toString(16).substr(0, 6) + `;fill:none;stroke-width:1" />
+			<polyline points="`
+			//If the new polyline colour matches the background, change the colour of the background.
+			if (Number(HexIndex * 16777215).toString(16).substr(0, 6) == BGColour) {
+				BGColour = "#" + Math.floor(Math.random() * 16777215).toString(16);
+			}
+		}
 		var CurrentLinePos = (HexIndex);
 		var FixedCurrentHexBlock = CurrentHexBlock;
-		while (FixedCurrentHexBlock.length < 8) {
+		while (FixedCurrentHexBlock.length < 4) {
 			FixedCurrentHexBlock += "0";
 		}
-		OutSVG += `
-		<line x1="` + CurrentLinePos  + `" y1="257" x2="` + CurrentLinePos + `" y2="` + (257 - parseInt(Number("0x" + FixedCurrentHexBlock.substring(0, 2)), 10)) + `" style="stroke:#` + FixedCurrentHexBlock.substring(2) + `; stroke-width:1">
-		</line>`;
-	});
-	OutSVG += `
-	</svg>`
-	fs.writeFileSync( 'out.svg', OutSVG );
+		OutSVG += (parseInt(Number("0x" + FixedCurrentHexBlock.substring(0, 2)), 10) * 10) + "," + (parseInt(Number("0x" + FixedCurrentHexBlock.substring(2)), 10) * 10) + " ";
+	}
+	//End of the SVG file.
+	OutSVG += `" style="stroke:#` + Number(HexString.length * 16777.215).toString(16).substr(0, 6) + `;fill:none;stroke-width:1" />
+	</svg>`;
+	fs.writeFileSync( 'out.svg', OutSVG.replace("{BGColour}", BGColour));
 }
 
 function DecodeSVG(InputFilePath) {
