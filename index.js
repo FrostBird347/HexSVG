13a0fe5be6caf1666313ddd79564284c9d4151aa
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2021-01-18 12:34:32 +0800

    Added decoder
    Also changed the encoder slightly.

--- index.js
+++ index.js
@@ -34,30 +34,27 @@ function EncodeSVG(InputFilePath) {
 		console.error(err);
 		process.exit(1);
 	}
-	//Convert the file to a hex string and create the start of the SVG.
+	//Convert the file to a hex string and create the start of the SVG
 	var HexString = InputData.toString('hex');
+	var FileNameRegex = /[^\.]+$/gi;
 	var OutSVG = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-	<svg height="2560" width="2560" viewBox="2560, 2560" xmlns="http://www.w3.org/2000/svg" style="background:{BGColour}">
+<svg height="2560" width="2560" viewBox="2560, 2560" xmlns="http://www.w3.org/2000/svg" style="background:transparent">
+	<!--` + FileNameRegex.exec(InputFilePath).toString() + `-->
 	<polyline points="`;
-	var BGColour = "#" + Math.floor(Math.random() * 16777215).toString(16).substr(4).repeat(4);
 	var SizeCounter = 0;
 	var CurrentHexBlock = ""
 	var HexIndex = 0
-	//Go through every 4 characters, convert them into a decimal number and add the points to a polyline.
+	//Go through every 4 characters, convert them into a decimal number and add the points to a polyline
 	for (var i = 0, charsLength = HexString.length; i < charsLength; i += 4) {
-		//I earlier split the string into a massive array and went through that.
-		//This method saves a ton of memory.
+		//I earlier split the string into a massive array and went through that
+		//This method saves a ton of memory
 		CurrentHexBlock = HexString.substring(i, i + 4);
 		HexIndex = i / 4
 		SizeCounter++;
-		//Split the polyline every 1000 characters to reduce lag.
+		//Split the polyline every 1000 characters to reduce lag
 		if (SizeCounter % 250 == 0) {
 			OutSVG += `" style="stroke:#` + Number(HexIndex * 16777215).toString(16).substr(0, 6) + `;fill:none;stroke-width:1" />
-			<polyline points="`
-			//If the new polyline colour matches the background, change the colour of the background.
-			if (Number(HexIndex * 16777215).toString(16).substr(0, 6) == BGColour) {
-				BGColour = "#" + Math.floor(Math.random() * 16777215).toString(16);
-			}
+	<polyline points="`
 		}
 		var CurrentLinePos = (HexIndex);
 		var FixedCurrentHexBlock = CurrentHexBlock;
@@ -66,13 +63,44 @@ function EncodeSVG(InputFilePath) {
 		}
 		OutSVG += (parseInt(Number("0x" + FixedCurrentHexBlock.substring(0, 2)), 10) * 10) + "," + (parseInt(Number("0x" + FixedCurrentHexBlock.substring(2)), 10) * 10) + " ";
 	}
-	//End of the SVG file.
+	//End of the SVG file
 	OutSVG += `" style="stroke:#` + Number(HexString.length * 16777.215).toString(16).substr(0, 6) + `;fill:none;stroke-width:1" />
-	</svg>`;
-	fs.writeFileSync( 'out.svg', OutSVG.replace("{BGColour}", BGColour));
+</svg>`;
+	fs.writeFileSync( 'encoded.svg', OutSVG);
 }
 
 function DecodeSVG(InputFilePath) {
-	console.error("I have not made the decoder yet!");
-	process.exit(1);
+	try {
+		var InputData = fs.readFileSync(InputFilePath);
+	} catch(err) {
+		console.error("Failed to read file!");
+		console.error(err);
+		process.exit(1);
+	}
+	var InputString = InputData.toString();
+	//Check if SVG is the correct size and format
+	if (!InputString.startsWith(`<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg height="2560" width="2560" viewBox="2560, 2560" xmlns="http://www.w3.org/2000/svg"`)) {
+		console.error("Invalid format!");
+		process.exit(1);
+	}
+	//Regex to get points list
+	var DataRegex = /(?<=<polyline points=")(.*?)(?= " style=")/gm;
+	//Remove text between points for large files
+	FixedInput = InputString.replace(/(" style="stroke:#?([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2});fill:none;stroke-width:1" \/>\s\s<polyline points=")/gm, '')
+	//Create array of points
+	var DataArray = DataRegex.exec(FixedInput).toString().replace(/ /g, ',').split(',');
+	var DecodedFile = Array(DataArray.length);
+	//Convert each point into valid hex
+	DataArray.forEach((DecimalData, Index) => {
+		DecodedFile[Index] = (parseInt(DecimalData) / 10).toString(16);
+		while (DecodedFile[Index].length < 2) {
+			DecodedFile[Index] = "0" + DecodedFile[Index]
+		}
+	});
+	//Get the decoded file's extension
+	var FileExtensionRegex = /(?<=	<!--)(.*?)(?=-->)/gm;
+	var FileExtension = FileExtensionRegex.exec(InputString)[0];
+	//Convert hex to a buffer and save
+	fs.writeFileSync( 'decoded.' + FileExtension, Buffer.from(DecodedFile.toString().replace(/,/g, ''), "hex"));
 }
